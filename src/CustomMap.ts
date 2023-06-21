de33ce7309730a071a93f15886c419be2baa26ff
Rosina Scampino <rosinascampino@gmail.com>
2023-06-21 14:39:49 -0400

    marker content

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -3,7 +3,8 @@ interface Mappable{
   location: {
     lat: number;
     lng: number
-  }
+  };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -11,7 +12,7 @@ export class CustomMap {
 
   constructor(divId: string){
     this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-      zoom: 2,
+      zoom: 1,
       center: {
         lat: 0,
         lng: 0
@@ -30,7 +31,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hello!'
+        content: mappable.markerContent()
       });
 
       infoWindow.open(this.googleMap, marker);
